a1531d8c55809446a5b083b59f7609e1af34ec43
Filchakov Denys <filchakov.denis@gmail.com>
2016-09-22 22:27:40 +0300

    fix kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        //$schedule->command('queue:work')->withoutOverlapping()->everyMinute();
-        //$schedule->command('queue:work')->withoutOverlapping()->everyMinute();
+        $schedule->command('queue:work')->withoutOverlapping()->everyMinute();
+        $schedule->command('queue:work')->withoutOverlapping()->everyMinute();
+        $schedule->command('queue:work')->withoutOverlapping()->everyMinute();
 
         // $schedule->command('inspire')
         //          ->hourly();
